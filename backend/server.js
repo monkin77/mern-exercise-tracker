136ecb2675161c2cf42ead2aceeda8c2fbf4b5b2
monkin77 <monkinsmurf@gmail.com>
2021-02-17 13:29:20 +0000

    First commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');   // help us connect to our mongodb database
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established");
+})
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);     // when someone goes into the rootUrl/exercises it is going to load everything in the exercisesRouter
+app.use("/users", usersRouter);     // usersRouter will be routes inside the url localhost:5000/users
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
